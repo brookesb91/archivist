68de272044f2c55b3fa62888271a58471b98f285
Ben Brookes <ben@pixelbeard.co>
2020-07-11 00:01:55 +0100

    Update chan.service.js

--- src/services/chan.service.js
+++ src/services/chan.service.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
+const getUrl = (board, thread) => `https://a.4cdn.org/${board}/thread/${thread}.json`;
+
 export const ChanService = {
   async getThread(board, thread) {
+    const url = getUrl(board, thread);
+
     try {
-      const url = `https://a.4cdn.org/${board}/thread/${thread}.json`;
       const res = await axios.get(url, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
+
       return res.data;
+
     } catch (e) {
       throw new Error(e);
     }
